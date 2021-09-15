92ea3cde0d2eabf87d1a07c2b231c487b20abaf4
irfanwashere <irfan.a@allentics.com>
2021-09-15 14:19:43 +0530

    modify base url

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,2 +1,8 @@
-export const WEB_API_URL = 'https://oyedost.herokuapp.com:5000/api';
+// export const WEB_API_URL = 'http://localhost:5000/api';
 
+const baseUrl =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:5000'
+    : 'https://oyedost.herokuapp.com';
+
+export const WEB_API_URL = `${baseUrl}/api`;
