1fb0c01d34988a1f338926f90cf221cf354d547b
Irfan Ansari <irfanas542@gmail.com>
2021-09-15 13:52:32 +0530

    Update config.js
    added port in heroku app

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,4 @@
 // export const WEB_API_URL = 'http://localhost:5000/api';
-export const WEB_API_URL = 'https://oyedost.herokuapp.com/api';
+export const WEB_API_URL = 'https://oyedost.herokuapp.com:5000/api';
+
 
